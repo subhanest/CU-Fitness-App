0db7a8eea3c1d368065daa184834586d0ac68c96
ahmed-ahsan01 <ahsan.ashraf56@gmail.com>
2025-02-11 20:15:13 -0500

    fix sign in/up button and added pages redirect button below

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path  # type: ignore
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Home Page
+    path('explore/', views.explore, name='explore'),  # Explore Page
+    path('fitness/', views.fitness, name='fitness'),  # Fitness Page
+    path('nutrition/', views.nutrition, name='nutrition'),  # Nutrition Page
+    path('privacy-settings/', views.settings, name='settings'),  # Settings Page
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render  # type: ignore
+from django.utils.timezone import now  # type: ignore
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'myapp/index.html')
+    return render(request, 'myapp/index.html', {'timestamp': now().timestamp()})
+
+def explore(request):
+    return render(request, 'myApp/explore.html')  # Explore Page
+
+def fitness(request):
+    return render(request, 'myApp/fitness.html')  # Fitness Page
+
+def nutrition(request):
+    return render(request, 'myApp/nutrition.html')# Nutrition Page
+
+def settings(request):
+    return render(request, 'myApp/settings.html') # Settings Page

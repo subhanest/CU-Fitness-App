7eda8361d2f8d3f3a8a8ac5610cf233e3d9c4830
taf-debug <hrtafsir1998@gmail.com>
2025-02-14 00:10:30 -0500

    Initial commit

--- allapps/urls.py
+++ allapps/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),  # Link to the home view or any other view
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- allapps/views.py
+++ allapps/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'allapps/ER.html')
+
+
+
+
+
+
+
+
+    

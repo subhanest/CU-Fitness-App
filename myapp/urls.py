f028184ab2c629b55217fcab511f0c3684caf21d
ifthakhar-parvez <ifthakharprottoy@gmail.com>
2025-02-15 16:02:00 -0500

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home Page
     path('explore/', views.explore, name='explore'),  # Explore Page
-    path('fitness/', views.fitness, name='fitness'),  # Fitness Page
     path('privacy-settings/', views.settings, name='settings'),  # Settings Page
     
 ]

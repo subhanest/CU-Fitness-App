32265f37b67713348a4578a1ed124d7f37d4a616
subhanest <63142162+subhanest@users.noreply.github.com>
2025-02-15 16:38:45 -0500

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('explore/', views.explore, name='explore'),  # Explore Page
     path('fitness/', views.fitness, name='fitness'),  # Fitness Page
     path('privacy-settings/', views.settings, name='settings'),  # Settings Page
-    
+    path('sign__up/', views.signup_view, name='sign__up'),  
+    path('log__in/', views.login_view, name='log__in')
 ]
 

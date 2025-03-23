ac47ec7015f2a04399251f286570ad532048b2c9
ArnoByte <arnobislam252@gmail.com>
2025-03-23 06:33:42 -0400

    Update urls.py

--- cufitness/urls.py
+++ cufitness/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+# Create a router for the viewsets
+router = DefaultRouter()
+router.register(r'workout-plans', views.WorkoutPlanViewSet)
+router.register(r'nutrition-plans', views.NutritionPlanViewSet)
+router.register(r'progress-trackers', views.ProgressTrackerViewSet)
+router.register(r'user-profiles', views.UserProfileViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),

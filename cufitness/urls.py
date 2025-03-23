0a2b8ab0d90a2cf28af4efbee90ac7d333b3b5a2
ArnoByte <arnobislam252@gmail.com>
2025-03-23 08:14:05 -0400

    Update urls.py

--- cufitness/urls.py
+++ cufitness/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from exercise import views
 from rest_framework.routers import DefaultRouter
 
 # Create a router for the viewsets

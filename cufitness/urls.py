319e52237d46ae30889594825404bed48a4ab52b
ifthakhar-parvez <ifthakharprottoy@gmail.com>
2025-03-23 21:01:34 -0400

    Update urls.py

--- cufitness/urls.py
+++ cufitness/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from exercise import views
-from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()

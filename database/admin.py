3b883ea8637d0c4612b49d9ebc007504c68e9e0d
ArnoByte <arnobislam252@gmail.com>
2025-04-13 07:41:47 -0400

    Delete database directory

--- database/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, UserQuestionnaire, UserSession
-
-admin.site.register(CustomUser)
-admin.site.register(UserQuestionnaire)
-admin.site.register(UserSession)

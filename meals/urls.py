74362bb531b6014a38a1d2c5b5bb41028ccae5f2
ArnoByte <arnobislam252@gmail.com>
2025-04-13 07:08:47 -0400

    Delete meals directory

--- meals/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views  # Import views from the same app
-
-urlpatterns = [
-     path('', views.nutrition_index, name='Nutrition'),
-     path('chatbot/', views.chatbot_response, name='chatbot'),
-]
